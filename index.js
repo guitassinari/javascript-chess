859d69951a7bc441e5b31936abdea097ea199034
Guilherme Tassianri <guitassinari@gmail.com>
2020-01-18 02:37:56 -0300

    begins creating chess game

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+app.post('/move', function (req, res) {
+  console.log(req)
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
