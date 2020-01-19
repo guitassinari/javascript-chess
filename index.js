7dddde3ca63db0af7ccdcd3a9156d2d9972564de
Guilherme Tassinari <guitassinari@gmail.com>
2020-01-19 00:07:28 -0300

    Draws initial game board (#1)
    * begins creating chess game
    
    * send initial board
    
    * Adds pieces to chess board

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const ChessBoard = require('./js/ChessBoard')
+const ChessGame = require('./js/ChessGame')
 
 app.use(express.static(__dirname+'/css'))
+app.use(express.static(__dirname+'/js'))
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get('/new', function (req, res) {
+  const board = ChessBoard.initialBoard(ChessGame.TEAMS().WHITE, ChessGame.TEAMS().BLACK)
+  const object = board.allPositions().reduce((positionObject, position) => {
+    const piece = board.getPieceAt(position)
+    positionObject[position.toString()] = [piece.name(), piece.team]
+    return positionObject
+  }, {})
+
+  res.json(object);
+});
+
+app.post('/move', function (req, res) {
+  console.log(req)
 });
 
 app.listen(3000, function () {
---
+++ js/ChessGame.js
@@ -0,0 +1,40 @@
+class ChessGame {
+  constructor(chessBoard) {
+    this.board = chessBoard
+    this.currentTeam = TEAMS.WHITE
+  }
+
+  static TEAMS() {
+    return { BLACK: "black", WHITE: "white" };
+  }
+
+  movePiece(from, to) {
+    const piece = this.board.getPieceAt(from)
+    if(piece.can_move(from, to) === false) {
+      throw new Error("Impossivel mover esta peça para a posição destino")
+    }
+
+    if(piece.team() !== this.currentTeam) {
+      throw new Error("Impossível mover a peça da outra equipe")
+    }
+
+    const pieceAtDestination = this.board.getPieceAt(to)
+    if(piece.team() === pieceAtDestination.team()) {
+      throw new Error("Impossivel mover esta peça para a posição destino pois há outra peça nesta posição.")
+    }
+
+    this.board.removePieceAt(to)
+    this.board.movePiece(from, to)
+    this.__changeCurrentTeam()
+  }
+
+  __changeCurrentTeam() {
+    if(this.currentTeam === this.TEAMS.BLACK) {
+      this.currentTeam = this.TEAMS.WHITE
+    } else {
+      this.currentTeam = this.TEAMS.BLACK
+    }
+  }
+}
+
+module.exports = ChessGame
